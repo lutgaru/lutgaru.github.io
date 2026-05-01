e39399605afaf8888ad12ee69bf065441f0ff3b0
Adrian Neftali Sanchez <lutgaru@gmail.com>
2026-05-01 12:11:12 -0700

    update site to main domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
-  site: 'https://lutgaru.github.io',
+  site: 'https://lutgaru.com',
   base : '/',
   vite: {
     plugins: [tailwindcss()],
